317ddb8164e0f1354155606c3e2189f7ab583881
prokawsar <prokawsar@gmail.com>
2019-11-26 19:55:27 +0600

    destructuring variable

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ import React from 'react';
 import App from './src/App';
 
 export default function DailyActivityChart(props){
+  let {data, specificMonth, specificYear, color} = props
+
   return (
-    <App data={props.data} specificMonth={props.specificMonth} specificYear={props.specificYear}/>
+    <App data={data} specificMonth={specificMonth} specificYear={specificYear} color={color} />
   )
 
 }
