76c57c0c41750212a32524d81ae81aab62be886e
prokawsar <prokawsar@gmail.com>
2019-11-26 20:16:43 +0600

    theming working, variable renamed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Text, View, StyleSheet} from 'react-native';
+import {hexToRGB, RGBToHSL} from './ColorConverter';
 
 const styles = StyleSheet.create({
   days: {
@@ -19,31 +20,42 @@ const styles = StyleSheet.create({
 
 const daysInMonth = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
-const activityLevelColor = (level) => {
-  if(level == 0 || level == null) return '#E7DAEC'
-  if(level < 5) return '#AF7AC5'
-  if(level < 10) return '#9B59B6'
-  return '#884EA0'
+const activityLevelColor = (level, color) => {
+    color = color ? color : '#884EA0'
+    let theme = RGBToHSL(hexToRGB(color)),
+        {h, s, l} = theme;
+
+    if(level == 0 || level == null) return '#E4E6F3'
+    if(level < 5) return `hsl(${h},${s}%,${l+25}%)`
+    if(level < 10) return `hsl(${h},${s}%,${l+10}%)`
+    return `hsl(${h},${s}%,${l}%)`
+
+    // if(level == 0 || level == null) return '#E4D4EA' //'#E7DAEC'
+    // if(level < 5) return '#CB94E3'
+    // if(level < 10) return '#9B59B6'
+    // return '#884EA0'
+
 
 }
 
 const Month = (props) => {
   let now = new Date(),
-      { frequency, specificMonth, specificYear } = props,
+      { frequency, specificMonth, specificYear, color } = props,
       days = new Date(now.getFullYear(), now.getMonth()+1, 0).getDate(),
       monthChart = new Array(specificMonth ? daysInMonth[specificMonth] : days).fill(1).map((v, i) => `${specificYear ? specificYear : now.getFullYear()}/${specificMonth ? specificMonth : now.getMonth() + 1}/${v + i}` )
 
   return monthChart.map((day, key) => (
-    <View style={[styles.day, {backgroundColor: activityLevelColor(frequency[day])}]} key={key} />
+    <View style={[styles.day, {backgroundColor: activityLevelColor(frequency[day], color)}]} key={key} />
   ))
 
 }
 
 export default function App(props) {
+  let {data, specificMonth, specificYear, color} = props
   return (
     <View>
       <View style={styles.days}>
-        <Month frequency={props.data} specificMonth={props.specificMonth} specificYear={props.specificYear}/>
+        <Month frequency={data} specificMonth={specificMonth} specificYear={specificYear} color={color} />
       </View>
     </View>
   )
